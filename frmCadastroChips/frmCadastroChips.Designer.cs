b434be4b9874aca8d4c6f4f3e4728a9fca8e253d
Lukas Roberto <Lukas.roberto@hormail.com>
2016-06-13 20:24:21 -0300

    Add project files.

---
+++ frmCadastroChips/frmCadastroChips.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace frmCadastroChips
+{
+    partial class frmCadastrarChips
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lblCodigoChip = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblClienteInstalado = new System.Windows.Forms.Label();
+            this.lblDataEnvio = new System.Windows.Forms.Label();
+            this.lblOperadora = new System.Windows.Forms.Label();
+            this.lblImei = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(14, 293);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(138, 49);
+            this.btnCadastrar.TabIndex = 0;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(158, 293);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(138, 49);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // lblCodigoChip
+            // 
+            this.lblCodigoChip.AutoSize = true;
+            this.lblCodigoChip.Location = new System.Drawing.Point(112, 76);
+            this.lblCodigoChip.Name = "lblCodigoChip";
+            this.lblCodigoChip.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigoChip.TabIndex = 2;
+            this.lblCodigoChip.Text = "Codigo";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(112, 208);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblClienteInstalado
+            // 
+            this.lblClienteInstalado.AutoSize = true;
+            this.lblClienteInstalado.Location = new System.Drawing.Point(64, 181);
+            this.lblClienteInstalado.Name = "lblClienteInstalado";
+            this.lblClienteInstalado.Size = new System.Drawing.Size(85, 13);
+            this.lblClienteInstalado.TabIndex = 4;
+            this.lblClienteInstalado.Text = "Cliente Instalado";
+            // 
+            // lblDataEnvio
+            // 
+            this.lblDataEnvio.AutoSize = true;
+            this.lblDataEnvio.Location = new System.Drawing.Point(74, 155);
+            this.lblDataEnvio.Name = "lblDataEnvio";
+            this.lblDataEnvio.Size = new System.Drawing.Size(75, 13);
+            this.lblDataEnvio.TabIndex = 5;
+            this.lblDataEnvio.Text = "Data de Envio";
+            // 
+            // lblOperadora
+            // 
+            this.lblOperadora.AutoSize = true;
+            this.lblOperadora.Location = new System.Drawing.Point(92, 128);
+            this.lblOperadora.Name = "lblOperadora";
+            this.lblOperadora.Size = new System.Drawing.Size(57, 13);
+            this.lblOperadora.TabIndex = 6;
+            this.lblOperadora.Text = "Operadora";
+            // 
+            // lblImei
+            // 
+            this.lblImei.AutoSize = true;
+            this.lblImei.Location = new System.Drawing.Point(123, 102);
+            this.lblImei.Name = "lblImei";
+            this.lblImei.Size = new System.Drawing.Size(26, 13);
+            this.lblImei.TabIndex = 7;
+            this.lblImei.Text = "Imei";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "CLARO",
+            "VIVO",
+            "TIM (DATORA)"});
+            this.comboBox1.Location = new System.Drawing.Point(155, 125);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(155, 152);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(155, 178);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(155, 205);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // frmCadastrarChips
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 371);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblImei);
+            this.Controls.Add(this.lblOperadora);
+            this.Controls.Add(this.lblDataEnvio);
+            this.Controls.Add(this.lblClienteInstalado);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblCodigoChip);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Name = "frmCadastrarChips";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Chips";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lblCodigoChip;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblClienteInstalado;
+        private System.Windows.Forms.Label lblDataEnvio;
+        private System.Windows.Forms.Label lblOperadora;
+        private System.Windows.Forms.Label lblImei;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ frmCadastroChips/frmCadastroChips.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmCadastroChips
+{
+    public partial class frmCadastrarChips : Form
+    {
+        public frmCadastrarChips()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblCodigoChip.Text = "Lukas";
+        }
+
+    }
+}
